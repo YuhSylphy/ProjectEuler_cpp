a9155b0715cf0b148f184ff7ee74e907b52cdf6e
Yuh Sylphy <YuhSylphy@gmail.com>
2012-12-04 19:53:42 +0900

    fixed

--- problem016.cpp
+++ problem016.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
 
 	std::vector<std::string> state = 
 	{
-		"Problem 15",
+		"Problem 16",
 		"",
 		"2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.",
 		"",
